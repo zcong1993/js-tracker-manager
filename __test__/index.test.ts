c23e93fbecc8da9c989e1427a30b460e5df00950
zcong1993 <zhangcong1992@gmail.com>
2020-05-28 15:03:09 +0800

    feat: add example and test

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,3 +1,227 @@
-it('should work well', () => {
-  expect(true).toBe(true)
+import {
+  TrackerManager,
+  createViewTracker,
+  createClickTracker,
+  createDurationTracker,
+} from '../src'
+
+const toData = (obj: any) => {
+  return JSON.parse(JSON.stringify(obj))
+}
+
+const expectDuration = (act: any, exp: any, deviation: number = 0.01) => {
+  expect(act.ext.duration).toBeDefined()
+  expect(exp.ext.duration).toBeDefined()
+  const dev = Math.abs((exp.ext.duration - act.ext.duration) / act.ext.duration)
+  expect(dev).toBeLessThan(deviation)
+  delete act.ext.duration
+  delete exp.ext.duration
+  expect(act).toEqual(exp)
+}
+
+const expectDurationArray = (
+  acts: any[],
+  exps: any[],
+  deviation: number = 0.01
+) => {
+  expect(acts.length).toBe(exps.length)
+  for (let i = 0; i < acts.length; i++) {
+    expectDuration(acts[i], exps[i], deviation)
+  }
+}
+
+const sleep = (n: number) => new Promise((r) => setTimeout(r, n))
+
+it('view tracker should work well', () => {
+  const tm = new TrackerManager({ userId: 'xx' })
+
+  const vt1 = createViewTracker({
+    eventName: 'view',
+    screenName: 'presale',
+  })
+  tm.addViewTracker(vt1)
+
+  expect(tm.trackerSize).toBe(1)
+  expect(tm.unEndDurationTrackerSize).toBe(0)
+  expect(toData(tm.getTrackers())).toEqual([
+    { eventName: 'view', ext: { userId: 'xx' }, screenName: 'presale' },
+  ])
+  expect(tm.trackerSize).toBe(0)
+
+  for (let i = 0; i < 10; i++) {
+    const vt1 = createViewTracker({
+      eventName: 'view',
+    })
+    tm.addViewTracker(vt1)
+  }
+  expect(tm.trackerSize).toBe(10)
+  expect(tm.unEndDurationTrackerSize).toBe(0)
+  expect(toData(tm.getTrackers())).toEqual(
+    Array(10).fill({
+      eventName: 'view',
+      ext: { userId: 'xx' },
+      screenName: 'presale',
+    })
+  )
+  expect(tm.trackerSize).toBe(0)
+
+  // same eventId
+  for (let i = 0; i < 10; i++) {
+    const vt1 = createViewTracker({
+      eventId: 'xxx',
+      eventName: 'view',
+      screenName: 'presale',
+    })
+    tm.addViewTracker(vt1)
+  }
+  expect(tm.trackerSize).toBe(1)
+  expect(tm.unEndDurationTrackerSize).toBe(0)
+  expect(toData(tm.getTrackers())).toEqual(
+    Array(1).fill({
+      eventName: 'view',
+      ext: { userId: 'xx' },
+      screenName: 'presale',
+    })
+  )
+  expect(tm.trackerSize).toBe(0)
+})
+
+it('click tracker should work well', () => {
+  const tm = new TrackerManager({ userId: 'xx' })
+
+  const vt1 = createClickTracker({
+    eventName: 'click',
+    screenName: 'presale',
+  })
+  tm.addClickTracker(vt1)
+
+  expect(tm.trackerSize).toBe(1)
+  expect(tm.unEndDurationTrackerSize).toBe(0)
+  expect(toData(tm.getTrackers())).toEqual([
+    { eventName: 'click', ext: { userId: 'xx' }, screenName: 'presale' },
+  ])
+  expect(tm.trackerSize).toBe(0)
+
+  for (let i = 0; i < 10; i++) {
+    const vt1 = createViewTracker({
+      eventName: 'click',
+      screenName: 'presale',
+    })
+    tm.addClickTracker(vt1)
+  }
+  expect(tm.trackerSize).toBe(10)
+  expect(tm.unEndDurationTrackerSize).toBe(0)
+  expect(toData(tm.getTrackers())).toEqual(
+    Array(10).fill({
+      eventName: 'click',
+      ext: { userId: 'xx' },
+      screenName: 'presale',
+    })
+  )
+  expect(tm.trackerSize).toBe(0)
+
+  // same eventId
+  for (let i = 0; i < 10; i++) {
+    const vt1 = createViewTracker({
+      eventId: 'xxx',
+      eventName: 'click',
+      screenName: 'presale',
+    })
+    tm.addClickTracker(vt1)
+  }
+  expect(tm.trackerSize).toBe(1)
+  expect(tm.unEndDurationTrackerSize).toBe(0)
+  expect(toData(tm.getTrackers())).toEqual(
+    Array(1).fill({
+      eventName: 'click',
+      ext: { userId: 'xx' },
+      screenName: 'presale',
+    })
+  )
+  expect(tm.trackerSize).toBe(0)
+})
+
+it('duration tracker should work well', async () => {
+  const tm = new TrackerManager({ userId: 'xx' })
+  tm.setCurrentScreen('video')
+  const start = createDurationTracker({
+    type: 'start',
+    eventId: 'd-1',
+    eventName: 'playVideo',
+    // screenName: 'video',
+    time: 1209756949197785,
+  })
+  tm.addDurationTracker(start)
+
+  expect(tm.trackerSize).toBe(0)
+  expect(tm.unEndDurationTrackerSize).toBe(1)
+
+  const end = createDurationTracker({
+    type: 'end',
+    eventId: 'd-1',
+    eventName: 'playVideo',
+    time: 1209757949197785,
+  })
+  tm.addDurationTracker(end)
+
+  expect(tm.trackerSize).toBe(1)
+  expect(tm.unEndDurationTrackerSize).toBe(0)
+
+  expectDurationArray(toData(tm.getTrackers()), [
+    {
+      eventName: 'playVideo',
+      ext: { userId: 'xx', duration: '1.00' },
+      screenName: 'video',
+    },
+  ])
+
+  expect(tm.trackerSize).toBe(0)
+  expect(tm.unEndDurationTrackerSize).toBe(0)
+})
+
+it('split duration tracker should work well', async () => {
+  const tm = new TrackerManager({ userId: 'xx' })
+  const start = createDurationTracker({
+    type: 'start',
+    eventId: 'd-1',
+    eventName: 'playVideo',
+    screenName: 'video',
+  })
+  tm.addDurationTracker(start)
+
+  await sleep(1000)
+
+  expect(tm.trackerSize).toBe(0)
+  expect(tm.unEndDurationTrackerSize).toBe(1)
+
+  expectDurationArray(toData(tm.getTrackers()), [
+    {
+      eventName: 'playVideo',
+      ext: { userId: 'xx', duration: '1.00' },
+      screenName: 'video',
+    },
+  ])
+
+  expect(tm.trackerSize).toBe(0)
+  expect(tm.unEndDurationTrackerSize).toBe(1)
+
+  await sleep(1000)
+
+  tm.endDurationTracker('d-1')
+
+  expect(tm.trackerSize).toBe(1)
+  expect(tm.unEndDurationTrackerSize).toBe(0)
+
+  expectDurationArray(toData(tm.getTrackers()), [
+    {
+      eventName: 'playVideo',
+      ext: { userId: 'xx', duration: '1.00' },
+      screenName: 'video',
+    },
+  ])
+
+  expect(tm.trackerSize).toBe(0)
+  expect(tm.unEndDurationTrackerSize).toBe(0)
+
+  tm.endDurationTracker('d-2')
 })
--- src/example.ts
+++ src/example.ts
@@ -1,20 +1,3 @@
-# js-tracker-manager
-
-[![NPM version](https://img.shields.io/npm/v/@zcong/js-tracker-manager.svg?style=flat)](https://npmjs.com/package/@zcong/js-tracker-manager) [![NPM downloads](https://img.shields.io/npm/dm/@zcong/js-tracker-manager.svg?style=flat)](https://npmjs.com/package/@zcong/js-tracker-manager) [![CircleCI](https://circleci.com/gh/zcong1993/js-tracker-manager/tree/master.svg?style=shield)](https://circleci.com/gh/zcong1993/js-tracker-manager/tree/master) [![codecov](https://codecov.io/gh/zcong1993/js-tracker-manager/branch/master/graph/badge.svg)](https://codecov.io/gh/zcong1993/js-tracker-manager)
-
-> my cool project
-
-## Install
-
-```bash
-$ yarn add @zcong/js-tracker-manager
-# or npm
-$ npm i @zcong/js-tracker-manager --save
-```
-
-## Usage
-
-```ts
 import {
   TrackerManager,
   createClickTracker,
@@ -59,8 +42,3 @@ const main = async () => {
 }
 
 main()
-```
-
-## License
-
-MIT &copy; zcong1993
